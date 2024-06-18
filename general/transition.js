30580b18eba482525ce7966cc20cedcaf9c71321
avocadoozzz <164929816+avocadoozzz@users.noreply.github.com>
2024-06-19 02:16:21 +0300

    Delete general directory

--- general/transition.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("sign").addEventListener("click", function() {
-    window.location.href = "../authorization/regauto.html";
-  });
-
-  document.getElementById("aut").addEventListener("click", function() {
-    window.location.href = "../registr/reg.html";
-  });
